bdbcf33d15ead96c3938db21dfc02c69234d407c
adam-almeida <adamportifolio@gmail.com>
2022-07-20 21:58:18 -0300

    Finalizando estilo da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home(){
     return(
@@ -6,7 +7,18 @@ export default function Home(){
             <Head>
                 <title>Início | Ibi.News</title>
             </Head>
-            <h1>Hellow World</h1>
+
+            <main className={styles.contentContainer}>
+                <section className={styles.hero}>
+                    <span>Hey, sejam bem vindos</span>
+                    <h1>Notícias sobre o <span>Meu</span> mundo</h1>
+                    <p>Tenha acesso a todas as publicações<br/>
+                    <span>por R$ 9,90 por mês</span>
+                    </p>
+                </section>
+                <img src="/images/ilustration.svg" alt="Isa pensativa" />
+            </main>
+
         </>
     )
 }
