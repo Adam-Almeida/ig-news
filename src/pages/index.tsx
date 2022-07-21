97ba70f3556843c94a6adbb73d41d5402b5252f5
adam-almeida <adamportifolio@gmail.com>
2022-07-20 23:22:43 -0300

    Buscando o preço do produto na API do Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import SubscribeButton from '../components/SubscribeButton/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
-export default function Home(){
+interface HomeProps {
+    product: {
+        priceId: string,
+        amount: number
+    }
+}
+
+export default function Home({ product }: HomeProps){
+
     return(
         <>
             <Head>
@@ -14,13 +24,31 @@ export default function Home(){
                     <span>Hey, sejam bem vindos</span>
                     <h1>Notícias sobre o <span>Meu</span> mundo</h1>
                     <p>Tenha acesso a todas as publicações<br/>
-                    <span>por R$ 9,90 por mês</span>
+                    <span>por {product.amount} por mês</span>
                     </p>
-                    <SubscribeButton />
+                    <SubscribeButton priceId={product.priceId} />
                 </section>
                 <img src="/images/ilustration.svg" alt="Isa pensativa" />
             </main>
 
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const price = await stripe.prices.retrieve('price_1LNoD3H6trkR4HMSHOSgoCWx')
+
+    const product = {
+        priceId: price.id,
+        amount: new Intl.NumberFormat('pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format((price.unit_amount! / 100))
+    }
+
+    return {
+        props: {
+            product
+        }
+    }
 }
