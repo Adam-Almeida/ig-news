40db473ad03bb9dbacabc3879db316320f32694a
adam-almeida <adamportifolio@gmail.com>
2022-07-21 09:15:16 -0300

    Finalizando autenticação com next auth

--- src/components/SigInButton/index.tsx
+++ src/components/SigInButton/index.tsx
@@ -1,21 +1,32 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/react'
  
 import styles from './styles.module.scss'
 
 
 export default function SigInButton(){
     
-    const isUserLogged = false
+    const { data: session } = useSession()
 
-    return isUserLogged ? (
-        <button type="button" className={styles.sigInButton}>
+    console.log(session)
+
+    return session ? (
+        <button
+        type="button"
+        className={styles.sigInButton}
+        onClick={() => signOut()}
+        >
             <FaGithub color='#5cfb6c'/>
-            Adam Almeida
+            {session.user?.name}
             <FiX className={styles.closeIcon} color='#737380' />
         </button>
     ):(
-        <button type="button" className={styles.sigInButton}>
+        <button 
+            type="button"
+            className={styles.sigInButton}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color='#FD89BA'/>
             Entrar com o Gihtub
         </button>
